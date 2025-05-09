0d9ff4b327fe45777b3865b29dce13380cca320d
Luiz Carvalho <lucarval@redhat.com>
2025-05-09 13:39:34 -0400

    Mundo == World
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- main.go
+++ main.go
@@ -16,6 +16,6 @@ func main() {
 	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {   
-	fmt.Fprint(w, "Hello World!") 
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello Mundo!")
 }
